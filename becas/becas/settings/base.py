c4d209e9c1820671232a3d1c50f69ea1fb893eb4
Sotsil Silva <ada_hack127@hackademy.mx>
2022-08-05 18:25:26 -0500

    configurado settings, agredos dev y prod

--- becas/becas/settings/base.py
+++ becas/becas/settings/base.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-l4a5dibe-2%s&*c_h8a@-_5_7v)3yw%tgxm7w)&vcp5pwkc!&y
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -103,9 +103,9 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/4.1/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'es-MX'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'America/Mexico_City'
 
 USE_I18N = True
 
