39964954f9f9982d655b44dfdba69779fde8144e
Sotsil Silva <ada_hack127@hackademy.mx>
2022-08-07 06:43:09 -0500

    instalado DRF

--- becas/becas/settings/base.py
+++ becas/becas/settings/base.py
@@ -37,6 +37,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    #local apps
+    'rest_framework',
+    'initial'
 ]
 
 MIDDLEWARE = [
@@ -121,3 +124,9 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+#REST_FRAMEWORK = {
+ #   'DEFAULT_RENDERER_CLASSES':[
+  #      'rest_framework.renders.JSONRenderer',
+  #  ]
+#}
